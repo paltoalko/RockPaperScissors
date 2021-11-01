bb592d49a2040d9133284ed619b4950d8bafe8ef
paltoalko <martas90909@me.com>
2021-11-01 08:15:20 +0000

    Created a game that goes for 5 rounds

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const computerSelection = computerPlay()
 // console.log(computerSelection) // finaly returns a rock,paper or scissors
 
 function playerPlay() {
-    console.log("Let's play a Rock Paper Scissors! Type rock, scissors or paper.");
+    
     let playerChoice = prompt("Let's play a Rock Paper Scissors! Type rock, scissors or paper.")
     if (playerChoice.toLowerCase() === "rock") {
         playerChoice = "Rock";
@@ -25,43 +25,76 @@ function playerPlay() {
 }
 
 const playerSelection = playerPlay()
-// console.log(playerSelection)
-
+// user scores and computer score
+let userScore = 0;
+let computerScore = 0;
 
 // function that plays the game of rock paper and scissors
 
 function playRound(playerSelection,computerSelection) {
     if (computerSelection.toLowerCase() === playerSelection.toLowerCase() && computerSelection.toLowerCase() != "scissors" && playerSelection.toLowerCase() != "scissors") {
         console.log("It's a draw!");
-        return("Your Choice: " + playerSelection + ". " + "Computer: " + computerSelection);
+        userScore = userScore + 1;
+        computerScore = computerScore + 1;
+        return("Your Choice: " + playerSelection[0].toUpperCase() + playerSelection.substring(1) + ". " + "Computer: " + computerSelection);
     } else if (computerSelection.toLowerCase() === "rock" && playerSelection.toLowerCase() === "scissors") {
         console.log("Rock beats scissors! You lose :(");
-        return("Your Choice: " + playerSelection + ". " + "Computer: " + computerSelection);
+        computerScore = computerScore + 1;
+        return("Your Choice: " + playerSelection[0].toUpperCase() + playerSelection.substring(1) + ". " + "Computer: " + computerSelection);
      } else if (computerSelection.toLowerCase() === "scissors" && playerSelection.toLowerCase() === "rock") {
         console.log("Rock beats scissors! You win");
-        return("Your Choice: " + playerSelection + ". " + "Computer: " + computerSelection);
+        userScore = userScore + 1;
+        return("Your Choice: " + playerSelection[0].toUpperCase() + playerSelection.substring(1) + ". " + "Computer: " + computerSelection);
      } else if (computerSelection.toLowerCase() === "scissors" && playerSelection.toLowerCase() === "paper") {
         console.log("Scissors beats paper! You lose :(");
-        return("Your Choice: " + playerSelection + ". " + "Computer: " + computerSelection);
+        computerScore = computerScore + 1;
+        return("Your Choice: " + playerSelection[0].toUpperCase() + playerSelection.substring(1) + ". " + "Computer: " + computerSelection);
      } else if (computerSelection.toLowerCase() === "paper" && playerSelection.toLowerCase() === "scissors") {
         console.log("Scissors beats paper! You win");
-        return("Your Choice: " + playerSelection + ". " + "Computer: " + computerSelection);
+        userScore = userScore + 1; 
+        return("Your Choice: " + playerSelection[0].toUpperCase() + playerSelection.substring(1) + ". " + "Computer: " + computerSelection);
      } else if (computerSelection.toLowerCase() === "paper" && playerSelection.toLowerCase() === "rock") {
         console.log("Paper beats rock! You lose :(");
-        return("Your Choice: " + playerSelection + ". " + "Computer: " + computerSelection);
+        computerScore = computerScore + 1;
+        return("Your Choice: " + playerSelection[0].toUpperCase() + playerSelection.substring(1) + ". " + "Computer: " + computerSelection);
      } else if (computerSelection.toLowerCase() === "rock" && playerSelection.toLowerCase() === "paper") {
         console.log("Paper beats rock! You win");
-        return("Your Choice: " + playerSelection + ". " + "Computer: " + computerSelection);
+        userScore = userScore + 1;
+        return("Your Choice: " + playerSelection[0].toUpperCase() + playerSelection.substring(1) + ". " + "Computer: " + computerSelection);
      } else if (computerSelection.toLowerCase() === "scissors" && playerSelection.toLowerCase() === "scissors") {
          console.log("It's scissoring time!");
+         userScore = userScore + 1;
+         computerScore = computerScore + 1;
          return("Your choice to scissor with Computer was not wise. Maybe try with a person ;))" )
      } else {
-        console.log("I'm not sure what, but something went worng :(");
+        console.log("I'm not sure what, but something went wrong :(");
      }
 }
 
-console.log(playRound(playerSelection,computerSelection))
+
 
 // function that plays 5 rounds of RPS
+for(var i=0;i<5;i++){
+    console.log("Round ", i + 1);
+    let playerSelection = prompt("Let's play a Rock Paper Scissors! Type rock, scissors or paper.");
+    const computerSelection = computerPlay()
+    console.log(playRound(playerSelection, computerSelection))
+    console.log("Your score = " + userScore);
+    console.log("Computer's score = " + computerScore);
+  }
 
-// plays once and notes the score 
+// function that displays winner at the end
+
+function results(userScore,computerScore) {
+    if (computerScore > userScore) {
+        console.log("You lost :( " + "Your score: " + userScore + ". Computer's score: " + computerScore + ".")
+    } else if (computerScore == userScore) {
+        console.log("It's a tie! " + "Your score: " + userScore + ". Computer's score: " + computerScore + ".")
+    } else if (computerScore < userScore) {
+        console.log("Congrats! You won! " + "Your score: " + userScore + ". Computer's score: " + computerScore + ".")
+    }
+    
+}
+console.log(results(userScore,computerScore));
+
+
